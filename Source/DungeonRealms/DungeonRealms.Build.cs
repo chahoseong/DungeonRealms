4ce6c2abd64f9b64bd0ae87c951fb84ada435236
Cha Hoseong <hoseong@chahoseong.com>
2025-07-17 16:18:00 +0900

    Add root namespace

--- Source/DungeonRealms/DungeonRealms.Build.cs
+++ Source/DungeonRealms/DungeonRealms.Build.cs
@@ -8,9 +8,21 @@ public class DungeonRealms : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange([
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+			"EnhancedInput"
+		]);
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		PrivateDependencyModuleNames.AddRange([
+			
+		]);
+		
+		PublicIncludePaths.AddRange([
+			"DungeonRealms"
+		]);
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
